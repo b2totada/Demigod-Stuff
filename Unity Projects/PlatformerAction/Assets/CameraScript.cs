1be23547d6cf5c5f18580622eb831b095705c69d
b2totada <b2totada@gmail.com>
2021-05-01 23:58:04 +0200

    YouDied

--- Unity Projects/PlatformerAction/Assets/CameraScript.cs
+++ Unity Projects/PlatformerAction/Assets/CameraScript.cs
@@ -6,31 +6,30 @@ using UnityEngine.SceneManagement;
 public class CameraScript : MonoBehaviour
 {
     private Scene scene;
-    private Camera cam;
-    private int oldMask;
+    [HideInInspector] public Camera cam;
     public Sprite sunShine;
     public Sprite evening;
     public Sprite night;
     // Start is called before the first frame update
     void Start()
     {
-        scene = SceneManager.GetActiveScene();
         cam = GetComponent<Camera>();
-        int oldMask = cam.cullingMask;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level1") && transform.GetComponentInChildren<SpriteRenderer>().sprite != sunShine)
+        scene = SceneManager.GetActiveScene();
+
+        if (scene == SceneManager.GetSceneByName("Level1") && transform.GetComponentInChildren<SpriteRenderer>().sprite != sunShine)
         {
             transform.GetComponentInChildren<SpriteRenderer>().sprite = sunShine;
         }
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level2") && transform.GetComponentInChildren<SpriteRenderer>().sprite != evening)
+        if (scene == SceneManager.GetSceneByName("Level2") && transform.GetComponentInChildren<SpriteRenderer>().sprite != evening)
         {
             transform.GetComponentInChildren<SpriteRenderer>().sprite = evening;
         }
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level3") && transform.GetComponentInChildren<SpriteRenderer>().sprite != night)
+        if (scene == SceneManager.GetSceneByName("Level3") && transform.GetComponentInChildren<SpriteRenderer>().sprite != night)
         {
             transform.GetComponentInChildren<SpriteRenderer>().sprite = night;
         }
@@ -39,7 +38,6 @@ public class CameraScript : MonoBehaviour
     public void Darkness()
     {
         cam.cullingMask = 0;
-        
         Invoke("NoDarkness", 2);
     }
 
